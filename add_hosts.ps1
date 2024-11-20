cde418fb0180f4077934afe4fa6b03c90c8bfb72
Lucas08-ux <lgommar415@g.educaand.es>
2024-11-20 19:22:23 +0100

    hosts

---
+++ add_hosts.ps1
@@ -0,0 +1,4 @@
+# add_hosts.ps1
+$hostsPath = "C:\Windows\System32\drivers\etc\hosts"
+Add-Content -Path $hostsPath -Value "192.168.57.102  lucas"
+Add-Content -Path $hostsPath -Value "192.168.57.102  web2"
